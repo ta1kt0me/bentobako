9dd804f5881ac9bd0652552af1d0c97fa0baef47
ta1kt0me <p.wadachi@gmail.com>
2017-02-26 14:56:47 +0900

    refactor:extract set up mitamae logic

--- lib/mitamae_setupper.rb
+++ lib/mitamae_setupper.rb
@@ -13,7 +13,12 @@ module MitamaeSetupper
 
     def run
       Dir.mkdir SETUP_PATH unless Dir.exist? SETUP_PATH
+      setup_mitamae
+    end
 
+    private
+
+    def setup_mitamae
       File.open(MITAMAE_PATH, "wb") do |file|
         file.write open(LATEST_MITAMAE_URL).read
       end
