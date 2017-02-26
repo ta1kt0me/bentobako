6ad831e80c439cd7d46a0a3bf74fe20f0e21e7d4
ta1kt0me <p.wadachi@gmail.com>
2017-02-26 13:29:30 +0900

    Add minimum spec description

--- test/mitamae/setupper_test.rb
+++ test/mitamae/setupper_test.rb
@@ -5,7 +5,31 @@ class Mitamae::SetupperTest < Minitest::Test
     refute_nil ::Mitamae::Setupper::VERSION
   end
 
-  def test_it_does_something_useful
+  def test_that_create_setup_directory
+    assert false
+  end
+
+  def test_that_download_latest_mitamae_into_setup
+    assert false
+  end
+
+  def test_that_create_node_yaml_into_setup
+    assert false
+  end
+
+  def test_that_specify_user_into_node_user
+    assert false
+  end
+
+  def test_that_create_bootstrap_file_into_setup
+    assert false
+  end
+
+  def test_that_use_homebrew_as_package_manager
+    assert false
+  end
+
+  def test_that_setup_packages_for_rails
     assert false
   end
 end
